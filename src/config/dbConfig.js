e2c416549380521f3f0506fd77b6568b211438b7
Pradump2003 <pradump2003@gmail.com>
2024-12-27 20:08:12 +0530

    Added bd config for mongo

---
+++ src/config/dbConfig.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+import { NODE_ENV } from './serverConfig.js';
+import { DEV_DB_URL, PROD_DB_URL } from './serverConfig.js';
+export default async function connectDB() {
+  try {
+    if (NODE_ENV === 'development') {
+      await mongoose.connect(DEV_DB_URL);
+    } else if (NODE_ENV === 'production') {
+      await mongoose.connect(PROD_DB_URL);
+    }
+    console.log(`Connected to MongoDB database from ${NODE_ENV} environment`);
+  } catch (error) {
+    console.log('Error connecting to the database', error);
+  }
+}
